4849cf859ee6f308d023742da3baac938c57b3ce
Mateen Khan <mateenmk2428@gmail.com>
2025-02-25 14:40:47 +0530

    src/core/constants/directory.py updated

---
+++ src/core/constants/directory.py
@@ -0,0 +1,23 @@
+# src/constants/directory_constant.py is used to store directory related constant values 
+
+# Directory constants
+ARTIFACTS_DIR: str = 'artifacts'
+DATA_DIR: str = 'data'
+REPORTS_DIR: str = 'reports'
+OBJECTS_DIR: str = 'objects'
+
+# Sub-Data Directory constants
+RAW_DATA_DIR: str = 'raw'
+INTERIM_DATA_DIR: str = 'interim'
+PROCESSED_DATA_DIR: str = 'processed'
+SPLITTED_DATA_DIR: str = 'splitted'
+
+# Sub-Reports Directory constants
+VALIDATION_REPORT_DIR: str = 'validation'
+BEST_MODEL_PARAMS_DIR: str = 'params'
+BEST_MODEL_METRICS_DIR: str = 'metrics'
+EVALUATION_REPORT_DIR: str = 'evaluation'
+
+# Sub-Objects Directory constants
+PREPROCESSED_OBJECT_DIR: str = 'preprocessor'
+MODEL_OBJECT_DIR: str = 'model'
