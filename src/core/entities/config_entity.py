efdfdf1afaeac7ff357564dc20fe1a30badf1431
Mateen Khan <mateenmk2428@gmail.com>
2025-02-25 14:43:41 +0530

    src/core/entities/config_entity.py updated

---
+++ src/core/entities/config_entity.py
@@ -0,0 +1,18 @@
+import os
+
+from from_root import from_root
+from dataclasses import dataclass
+
+from src.core.constants.common import *
+from src.core.constants.directory import *
+from src.core.constants.data import *
+from src.core.constants.model import *
+
+
+# Data Ingestion Configuration
+@dataclass
+class DataIngestionConfig:
+    raw_data_dir = os.path.join(from_root(), ARTIFACTS_DIR, DATA_DIR, RAW_DATA_DIR)
+    interim_data_dir = os.path.join(from_root(),ARTIFACTS_DIR, DATA_DIR, INTERIM_DATA_DIR)
+    raw_file_path: str = os.path.join(raw_data_dir, DATA_INGESTION_RAW_FILE) 
+    data_file_path: str = os.path.join(interim_data_dir, DATA_INGESTION_DATA_FILE)
