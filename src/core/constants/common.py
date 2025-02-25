0ac3d16a6fb1c25e4505c7312189c5a5b1a97783
Mateen Khan <mateenmk2428@gmail.com>
2025-02-25 14:36:42 +0530

    src/core/constants/common.py updated

---
+++ src/core/constants/common.py
@@ -0,0 +1,15 @@
+# src/constants/common_constant.py is used to store common constant values 
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from.env file
+
+# Common constants
+TARGET_COLUMN: str = 'is_canceled'
+
+# MySQL constants
+MYSQL_ENGINE_URL = os.getenv('MYSQL_ENGINE_URL')
+DATABASE_NAME: str = 'projects_db'
+DATASET_NAME: str = 'bank_churn'
