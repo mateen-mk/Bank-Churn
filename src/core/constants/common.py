39a6ee6c11b9732a55316d54cca24d4ee2a3f644
Mateen Khan <mateenmk2428@gmail.com>
2025-02-25 14:37:24 +0530

    src/core/constants/common.py updated

--- src/core/constants/common.py
+++ src/core/constants/common.py
@@ -8,6 +8,8 @@ load_dotenv()  # Load environment variables from.env file
 
 # Common constants
 TARGET_COLUMN: str = 'is_canceled'
+SCHEMA_FILE_PATH = os.path.join("settings", "schema.yaml")
+
 
 # MySQL constants
 MYSQL_ENGINE_URL = os.getenv('MYSQL_ENGINE_URL')
