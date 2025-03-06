2369182ae77304ba6f0441ac05eb5ef39c594408
Mateen Khan <mateenmk2428@gmail.com>
2025-03-06 16:12:35 +0530

    .\src\core\utils\helpers.py json helper added

--- src/core/utils/helpers.py
+++ src/core/utils/helpers.py
@@ -3,10 +3,74 @@
 import os
 import sys
 
+import json
 import yaml
 import pandas as pd
 
 from src.core.exception import BankChurnException
+
+
+
+
+#----------------------------------------------------------------
+#------------------------ Json Helpers --------------------------
+#----------------------------------------------------------------
+
+# Function for reading JSON file from provided path
+@staticmethod
+def read_json(file_path: str) -> dict:
+    """
+    Read a JSON file and return its content as a dictionary.
+
+    Parameters:
+    file_path (str): The path to the JSON file to be read.
+
+    Returns:
+    dict: The content of the JSON file as a dictionary.
+
+    Raises:
+    BankChurnException: If an error occurs while reading the JSON file.
+    """
+    try:
+        with open(file_path, "r") as json_file:
+            return json.load(json_file)
+
+    except FileNotFoundError:
+        raise BankChurnException(f"Report file not found at {file_path}.", sys)
+
+    except json.JSONDecodeError:
+        raise BankChurnException("Failed to decode the report JSON file.", sys)
+
+    except Exception as e:
+        raise BankChurnException(f"Error in load_report: {str(e)}", sys) from e
+    
+
+# Function to write JSON file to provided path
+@staticmethod
+def write_json(file_path: str, data, replace: bool = False) -> None:
+    """
+    Write a dictionary to a JSON file.
+    
+    Parameters:
+    file_path (str): The path to the JSON file to be written.
+    data (object): The dictionary to be written to the JSON file.
+    replace (bool, optional): If True, overwrite the file if it already exists. Defaults to False.
+    
+    Raises:
+    BankChurnException: If an error occurs while writing the JSON file.
+    """
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        
+        with open(file_path, "w") as file:
+            json.dump(data, file, indent=4)
+    
+    except Exception as e:
+        raise BankChurnException(e, sys) from e
 
 
 
